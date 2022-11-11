c1b558d529741b314468eb72130469f2780b001c
dsvipeer <112516086+dsvipeer@users.noreply.github.com>
2022-11-11 13:07:20 -0300

    Add files via upload

---
+++ Rack-Unrackby-dsvipeer/mythic_progbar/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+ui_page('html/index.html') 
+
+client_scripts {
+    'client/main.lua'
+}
+
+files {
+    'html/index.html',
+    'html/css/style.css',
+    'html/js/script.js'
+}
+
+exports {
+    'Progress',
+    'ProgressWithStartEvent',
+    'ProgressWithTickEvent',
+    'ProgressWithStartAndTick'
+}
