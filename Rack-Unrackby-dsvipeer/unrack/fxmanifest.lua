965e06feb45d46c8c26b4172633004ba2aea2465
dsvipeer <112516086+dsvipeer@users.noreply.github.com>
2023-03-09 16:02:12 -0300

    Delete Rack-Unrackby-dsvipeer/unrack directory

--- Rack-Unrackby-dsvipeer/unrack/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-fx_version 'bodacious'
-games { 'gta5' }
-
-author 'https://github.com/dsvipeer'
-description 'A simple script that lets you rack/unrack big weapons from your car/police vehicle with proper animation to it.'
-version '1.0'
-
-client_script "client.lua"
